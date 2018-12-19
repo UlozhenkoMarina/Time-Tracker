ba7cd4313a49726dc3e51e20fc4527a92bbabc17
vonKleve <alas1633@gmail.com>
2018-12-19 12:45:00 +0200

    restored

--- time_tracker_web_app/src/main/java/com/timetracker/timet_racker_web_app/controller/UserController.java
+++ time_tracker_web_app/src/main/java/com/timetracker/timet_racker_web_app/controller/UserController.java
@@ -10,6 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.timetracker.timet_racker_web_app.dao.service.EventService;
+import com.timetracker.timet_racker_web_app.form.EventForm;
+import com.timetracker.timet_racker_web_app.model.Event;
+import com.timetracker.timet_racker_web_app.dao.service.GroupEventService;
+import com.timetracker.timet_racker_web_app.form.GroupEventForm;
+import com.timetracker.timet_racker_web_app.model.GroupEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.List;
 
 
@@ -52,7 +61,7 @@ public class UserController {
     @RequestMapping(value = { "/logout" }, method = RequestMethod.POST)
     public String logout(Model model, LoginForm form) {
         System.out.println(form.getEmail());
-        user = userService.getUser(form.getEmail(), form.getPassword());
+     //   user = userService.getUser(form.getEmail(), form.getPassword());
         user=null;
         return "loginPage";
     }
@@ -89,7 +98,7 @@ public class UserController {
     }
 
 
-    @RequestMapping(value = { "/userCabinetModify"}, method = RequestMethod.POST)
+    @RequestMapping(value = { "/userCabinet"}, method = RequestMethod.POST)
     public String userCabinetModify(Model model, RegisterForm form) {
 
         System.out.println(form.getEmail());
@@ -116,7 +125,7 @@ public class UserController {
 
 
 
-    @RequestMapping(value = { "/userCabinet"}, method = RequestMethod.POST)
+    @RequestMapping(value = { "/userCabinet"}, method = RequestMethod.GET)
     public String userCabinet(Model model, RegisterForm form) {
 
         System.out.println(form.getEmail());
@@ -166,7 +175,7 @@ public class UserController {
         users.setCountry(form.getCountry());
         users.setCity(form.getCity());
         model.addAttribute("contacts",userService.getContacts(users));
-        return "success";
+        return "contacts";
     }
 
     @RequestMapping(value="/addContacts",method = RequestMethod.POST)
@@ -184,12 +193,110 @@ public class UserController {
 
     //get user name by email
     //ad contact
+//
+//    @RequestMapping(value = "/getContacts", method = RequestMethod.GET)
+//    public String getContacts(Model model) {
+//        List<User> contacts = userService.getContactsUser(user);
+//        model.addAttribute("contacts", contacts);
+//        return "contacts";
+//    }
 
-    @RequestMapping(value = "/getContacts", method = RequestMethod.GET)
-    public String getContacts(Model model) {
-        List<User> contacts = userService.getContactsUser(user);
-        model.addAttribute("contacts", contacts);
-        return "contacts";
+
+    @Autowired
+    private EventService eventService;
+
+
+    @RequestMapping(value ="/CreateEventForm",method = RequestMethod.GET)
+    public String CreateEventForm(Model model){
+        EventForm form=new EventForm();
+        model.addAttribute("EventForm",form);
+        return "createEvent";
     }
 
+
+    @RequestMapping(value = "/createEvent",method = RequestMethod.POST)
+    public String createEvent(Model model, EventForm form) {
+        Event event = new Event();
+        //event.setCategoriesByCategory(form.getCategoriesByCategory());
+        //event.setUsersByUser(user);
+        event.setDate(form.getDate());
+        event.setDescription(form.getDescription());
+        //event.setDone(form.getDone());
+        event.setDone((byte)0);
+        event.setDuration(form.getDuration());
+        event.setEventNotesById(form.getEventNotesById());
+        event.setName(form.getName());
+        event.setPriority(form.getPriority());
+        eventService.addEvent(user,event,form.getCategoriesByCategory());
+        return "event";
+    }
+
+
+    @RequestMapping(value="/getEvents",method=RequestMethod.GET)
+    public String getEvents(Model model){
+        if (user!=null)
+            model.addAttribute("allEvents",eventService.getEvents(user));
+        return "event";
+    }
+
+    @RequestMapping(value="/getEventsByCategoryAndDate",method=RequestMethod.GET)
+    public String getEventsByCategoryAndDate(Model model, EventForm form){
+
+        model.addAttribute("EventsByCategoryAndDate",eventService.getEventsByCategoryAndDate(user,form.getCategoriesByCategory(),form.getDate()));
+        return "success";
+    }
+
+
+    @RequestMapping(value="/getEventsByCategory",method=RequestMethod.GET)
+    public String getEventsByCategory(Model model,EventForm form){
+        model.addAttribute("EventsByCategory",eventService.getEventsByCategory(user,form.getCategoriesByCategory()));
+        return "success";
+    }
+
+
+    @RequestMapping(value = "/getSystemCategories",method = RequestMethod.GET)
+    public String getSystemCategories(Model model){
+        model.addAttribute("systemCategories", eventService.getSystemCategories());
+        return "success";
+    }
+
+
+    @RequestMapping(value = "/getUserCategories",method = RequestMethod.GET)
+    public String getUserCategories(Model model){
+        model.addAttribute("userCategories", eventService.getUserCategories(user.getId()));
+        return "success";
+    }
+
+    @Autowired
+    private GroupEventService groupEventService;
+
+
+    @RequestMapping(value = "/showGroupEvents", method = RequestMethod.GET)
+    public String showGroupEvents(Model model) {
+        List<GroupEvent> groupEvents = groupEventService.getGroupEvents(user);
+        model.addAttribute("groupEvents", groupEvents);
+        return  "group-events";
+    }
+
+    @RequestMapping(value = "/showConfirmedGroupEvents", method = RequestMethod.GET)
+    public String showConfirmedGroupEvents(Model model) {
+        List<GroupEvent> groupEvents = groupEventService.getConfirmedGroupEvents(user);
+        model.addAttribute("groupEvents", groupEvents);
+        return  "";
+    }
+
+    @RequestMapping(value = "/showInvitations", method = RequestMethod.GET)
+    public String showInvitations(Model model) {
+        List<GroupEvent> groupEvents = groupEventService.getInvitations(user);
+        GroupEvent groupEvent = new GroupEvent();
+        model.addAttribute("groupEvents", groupEvents);
+        model.addAttribute("groupEvent", groupEvent);
+        return  "";
+    }
+
+    @RequestMapping(value = "/acceptInvitations", method = RequestMethod.POST)
+    public String acceptInvitations(Model model, GroupEvent event) {
+        groupEventService.acceptInvitation(user, event);
+        return  "";
+    }
 }
